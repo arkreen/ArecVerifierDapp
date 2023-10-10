ef7b12900204751a7bcc56552beb79217d6bfda4
秋秋 <973146586@qq.com>
2023-10-10 21:47:46 +0700

    Upload project code

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import '@/globals.css'
+import {NextUIProvider} from '@nextui-org/react'
+
+
+export default function MyApp({ Component, pageProps }) {
+    return <NextUIProvider><Component {...pageProps} /></NextUIProvider>
+  }
